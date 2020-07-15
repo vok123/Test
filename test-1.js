e6711d3a29ad8fa3f0fac84a41a3392c544cda67
vok123 <512855971@qq.com>
2020-07-15 18:40:32 +0800

    init

---
+++ test-1.js
@@ -0,0 +1,17 @@
+/** 给定一个数组, 要求取出数组内第二大的数值, 写两种方式(算法相同记为一种) */
+/**
+ * Input - [8, 1, 5, 7]
+ * Output - 7
+ */
+
+ /** 方式1 */
+function doIt1(arr) {
+
+}
+ /** 方式2 */
+function doIt2(arr) {
+
+}
+/** tests */
+console.log(doIt1([8, 1, 5, 7]) === 7);
+console.log(doIt2([8, 1, 5, 7]) === 7);
---
+++ test-2.js
@@ -0,0 +1,15 @@
+/** 给定一个数字数组, 要求返回除当前数字外的乘积数组 */
+/**
+ * Input - [1, 2, 3, 4]
+ * Output - [24, 12, 8, 6]
+ *
+ * Input - [0, 1, 3, 5]
+ * Output - [15, 0, 0, 0]
+ */
+
+function doIt(arr) {
+
+}
+/** tests */
+console.log(doIt([1, 2, 3, 4]).toString() === [24, 12, 8, 6].toString());
+console.log(doIt([0, 1, 3, 5]).toString() === [15, 0, 0, 0].toString());
